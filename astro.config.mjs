f092afd440831e85d11756c6d92e7eb41400a1b4
linapaper <linap.r71@gmail.com>
2024-11-27 09:53:22 -0500

    Fix netlify library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
