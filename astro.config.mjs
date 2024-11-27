8b7eca966cf21c50f307d3755845282374a4a766
linapaper <linap.r71@gmail.com>
2024-11-27 10:19:07 -0500

    Update Netlify configuration to use functions instead of edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
 });
