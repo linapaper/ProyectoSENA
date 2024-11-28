bce2ac719b6ba1bccd06d70b71476a9d4e13eee6
linapaper <linap.r71@gmail.com>
2024-11-28 08:52:15 -0500

    seo y posicionamiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://inlines.com.co',
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
+    })
+  ],
   output: 'server',
   adapter: vercel({
     analytics: true,
